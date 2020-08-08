203b6b5ca2698d37748463de028e8db7c28e5995
DEv-NE786 <69320234+DEv-NE786@users.noreply.github.com>
2020-08-08 05:24:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const dateFormat = require('dateformat');
 
 
 const prefix = "!"//لاتغير البرفكس
-const developers = ""//ايديك
+const developers = "726783431027064902"//ايديك
 
 ////////
 
@@ -1178,4 +1178,4 @@ const Sra7a = [
 });
 
 
-client.login('')//توكن بوتك
+client.login(process.env.TOKEN)//توكن بوتك
